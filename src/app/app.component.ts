7ae82618cfc836fc04ae9a67d54e759d60d35080
abiramibharanidharan <abiramibharanidharan@gmail.com>
2017-09-20 10:08:54 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+   FirstName:string
+   LastName:string
+   title:string
+  constructor(){}
+  ngOnInit()
+  {
+    this.FirstName='Abirami'
+    this.LastName='Bharanidharan'
+  }
+   displaytitle(first,last)
+   {
+     this.FirstName=first
+     this.LastName=last
+     this. title=first + " " + last
+    console.log(`Title is ${first} ${last}`)  
+   }
+  
 }
